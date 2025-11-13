23dc6619d0d17455e2498e9b9710b4a58d64a8a4
Philip Sanchez <philipsanchez14@csu.fullerton.edu>
2025-11-13 10:22:26 -0800

    working on testing the cart/checkout logic

--- Test.py
+++ Test.py
@@ -25,7 +25,30 @@ Menu
         _user = User(name, password)
         if not _user.login(name = name, password = password):
             continue
+        else :
+            while(True) :
+                print("""
+Menu:
+1. See Menu
+2. Place Order
+3. See Order History
+4. Exit """
+)
+                choice = input("Enter choice: ")
+                if choice == "1":
+                    with open("menuLists.json", 'r') as f :
+                        menu_list = json.load(f)
 
+                    print(menu_list)
+
+                elif choice == "2" :
+                    break
+
+                elif choice == "3" :
+                    break
+
+                elif choice == "4" :
+                    break 
     elif choice == "2":
         name = input("Enter username: ")
         password = input("Enter password: ")
